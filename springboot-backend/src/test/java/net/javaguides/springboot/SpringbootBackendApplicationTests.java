292a5b3c83d9529a8c59ea933c39006dc05fb5bd
Avik-Bera-scorpion23 <beraavik23@gmail.com>
2024-02-22 12:01:08 +0530

    Java minimal full stack project

---
+++ springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
